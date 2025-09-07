d03d523db460af7d62bee5911be784ba16db8b0f
minhhung1909 <hung19092003@gmail.com>
2025-09-08 00:19:07 +0700

    delete old folder

--- L6_Bit mask/bitmask.c
+++
@@ -1,8 +0,0 @@
-#include"stdio.h"
-
-int main(){
-    int a = 0b01;
-    int b = ~a;
-    printf("%d", b);
-    return 0;
-}
--- L7_Struct Union/test.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    unsigned int a = 65536;
-    unsigned short coercion = (unsigned int)a;
-    printf("value is: %d \n",a );
-    printf("coercion is: %d\n",coercion );
-    return 0;
-}
